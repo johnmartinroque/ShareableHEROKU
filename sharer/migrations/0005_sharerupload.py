f21b4b25d467ce8be6eceef35e9062ec4e51b1a6
johnmartinroque <johnmartinroque@gmail.com>
2024-04-19 22:46:40 +0800

    Change max_length from migration

--- sharer/migrations/0005_sharerupload.py
+++ sharer/migrations/0005_sharerupload.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('image', models.ImageField(blank=True, null=True, upload_to='uploads/images')),
-                ('description', models.CharField(max_length=10000000000)),
+                ('description', models.CharField(max_length=999)),
             ],
         ),
     ]

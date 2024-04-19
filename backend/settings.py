fe92af71f59ca8193cfadd9de233e4ea2dfb6d06
johnmartinroque <johnmartinroque@gmail.com>
2024-04-19 19:51:08 +0800

    heeee

--- backend/settings.py
+++ backend/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import os
 
 
+AWS_QUERYSTRING_AUTH = False
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
